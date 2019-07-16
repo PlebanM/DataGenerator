d56b94f5cd844d285ed7ca7da55dc57701948f48
Kamil Bednarczyk <kamil2.bednarczyk@gmail.com>
2019-07-16 15:58:21 +0200

    Create database context

---
+++ Models/SQLServerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataGenerator.Models
+{
+    public class SQLServerContext : DbContext
+    {
+        public SQLServerContext(DbContextOptions<SQLServerContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
