393fc4fc05110bc35d6cf357666962cee8f1712b
Kamil Bednarczyk <kamil2.bednarczyk@gmail.com>
2019-07-17 18:49:02 +0200

    Setup table generation initial logic

---
+++ Services/CSVColumnGenerator.cs
@@ -0,0 +1,25 @@
+﻿using DataGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataGenerator.Services
+{
+    public class CSVColumnGenerator
+    {
+        private SQLServerContext serverContext;
+        private DataContext dataContext;
+
+        public CSVColumnGenerator(SQLServerContext serverContext, DataContext dataContext)
+        {
+            this.serverContext = serverContext;
+            this.dataContext = dataContext;
+        }
+
+        internal List<string> GenerateColumn(Column columnStructure, long length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/CSVTableGenerator.cs
@@ -0,0 +1,50 @@
+﻿using CsvHelper;
+using DataGenerator.Models;
+using DataGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataGenerator.Services
+{
+    public class CSVTableGenerator
+    {
+        private CSVColumnGenerator columnGenerator;
+        public CSVTableGenerator(CSVColumnGenerator columnGenerator)
+        {
+            this.columnGenerator = columnGenerator;
+        }
+
+        public MemoryStream GenerateTable(DesiredTableStructure structure, Settings settings)
+        {
+            List<List<String>> columns = new List<List<String>>();
+            foreach (Column columnStructure in structure.Columns)
+            {
+                columns.Add(columnGenerator.GenerateColumn(columnStructure, settings));
+            }
+
+            MemoryStream table = new MemoryStream();
+            using (StreamWriter writer = new StreamWriter(table))
+            using (CsvWriter csvWriter = new CsvWriter(writer)) {
+                foreach (var columnName in structure)
+                {
+                    csvWriter.WriteField(columnName);
+                }
+                csvWriter.NextRecord();
+                for (int i = 0; i < settings.RowNumber; i++)
+                {
+                    for (int j = 0; j < columns.Count; j++)
+                    {
+                        csvWriter.WriteField(columns[j][i]);
+                    }
+                }
+                writer.Flush();
+                table.Position = 0;
+            }
+            return table;
+        }
+
+    }
+}
