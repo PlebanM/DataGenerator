73ea696f3dc1141fc864d89b2af5ee83f4060517
Marcin <pleban.marcin@gmail.com>
2019-07-19 01:13:28 +0200

    create type email

--- Services/CSVColumnGenerator.cs
+++ Services/CSVColumnGenerator.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DataGenerator.Services
@@ -35,6 +36,53 @@ namespace DataGenerator.Services
             generatorFunctions["lastName"] = GenerateLastNames;
             generatorFunctions["firstName"] = GenerateFirstNames;
             generatorFunctions["integer"] = GenerateIntegers;
+            generatorFunctions["email"] = GenerateEmails;
+
+        }
+
+        private List<string> GenerateEmails(Dictionary<string, int> options, long length)
+        {
+            List<String> result = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            Random rand = new Random();
+
+            int firstNameDBSize = dataContext.FirstNames.Count();
+            int lastNameDBSize = dataContext.LastNames.Count();
+            int domainsDBSize = dataContext.Domains.Count();
+            var firstnameDB = GenerateFirstNames(options, firstNameDBSize);
+            var lastNameDB = GenerateLastNames(options, lastNameDBSize);
+            var domainsDB = GenerateDomains(options, domainsDBSize);
+
+            int toSkipFirstName, toSkipLastName, toSkipDomain = 0;
+           
+            while (result.Count < length)
+            {
+                toSkipFirstName = rand.Next(0, firstNameDBSize);
+                toSkipLastName = rand.Next(0, lastNameDBSize);
+                toSkipDomain = rand.Next(0, domainsDBSize);
+
+                sb.Append(firstnameDB.ElementAt(toSkipFirstName));
+                sb.Append(".");
+                sb.Append(lastNameDB.ElementAt(toSkipLastName));
+                sb.Append("@");
+                sb.Append(domainsDB.ElementAt(toSkipDomain));
+
+                result.Add(sb.ToString());
+                sb.Clear();
+            }
+
+            return result;
+        }
+
+        public List<string> GenerateDomains(Dictionary<string, int> options, long length)
+        {
+            List<String> result = new List<string>();
+            while (result.Count < length)
+            {
+                result.AddRange(from domains in dataContext.Domains
+                                select domains.Name);
+            }
+            return result;
         }
 
         public List<string> GenerateLastNames(Dictionary<string, int> options, long length)
@@ -68,5 +116,7 @@ namespace DataGenerator.Services
             }
             return result;
         }
+
+
     }
 }
