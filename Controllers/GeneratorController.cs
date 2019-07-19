2a988e472653da58fafc5f10770a661cf65fcc95
kamilbednarczyk <43060715+kamilbednarczyk@users.noreply.github.com>
2019-07-19 09:34:59 +0200

    Revert "Send info of available types and options"

--- Controllers/GeneratorController.cs
+++ Controllers/GeneratorController.cs
@@ -4,13 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DataGenerator.Data;
 using DataGenerator.Models;
-using DataGenerator.Models.Options;
 using DataGenerator.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace DataGenerator.Controllers
 {
@@ -20,12 +17,10 @@ namespace DataGenerator.Controllers
     {
 
         private DataContext dataContext;
-        private OptionsContext optionsContext;
 
-        public GeneratorController(DataContext dataContext, OptionsContext optionsContext)
+        public GeneratorController(DataContext dataContext)
         {
             this.dataContext = dataContext;
-            this.optionsContext = optionsContext;
         }
         
         // GET: api/Generator
@@ -36,30 +31,6 @@ namespace DataGenerator.Controllers
             CSVTableGenerator tableGenerator = new CSVTableGenerator(columnGenerator);
             byte[] csvFile = tableGenerator.GenerateTable(generatorSetupData.tables[0], generatorSetupData.settings);
             return File(csvFile, "application/csv", "my_file.csv");
-        }
-
-        [HttpGet]
-        [Route("Options")]
-        public List<OptionsRepresentation> Options()
-        {
-            var types = optionsContext.ColumnTypes
-                .Include(ct => ct.ColumnTypeOptions)
-                .ThenInclude(cto => cto.Option)
-                .ToList();
-
-            List<OptionsRepresentation> representations = new List<OptionsRepresentation>();
-
-            foreach (var columnType in types)
-            {
-                List<string> options = new List<string>();
-                foreach (var option in columnType.ColumnTypeOptions.Select(e => e.Option))
-                {
-                    options.Add(option.Name);
-                }
-                representations.Add(new OptionsRepresentation { Type = columnType.type, Options = options });
-            }
-                                           
-            return representations;
         }
     }
 }
