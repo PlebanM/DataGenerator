f51ffae3ac3b77124e28a04d50c78bf429415a46
Kamil Bednarczyk <kamil2.bednarczyk@gmail.com>
2019-07-30 20:08:27 +0200

    Add model for relationships

---
+++ Models/Relationships/RelationshipEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataGenerator.Models.Relationships
+{
+    public class RelationshipEntity
+    {
+        public string TableName { get; set; }
+        public string ColumnName { get; set; }
+        public string Modality { get; set; }
+        public string Cardinality { get; set; }
+    }
+}
