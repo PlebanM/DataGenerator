d809459b7fc8a6d0a6c363e6e337bb0ef79922ec
Pleban Marcin <pleban.marcin@gmail.com>
2019-08-12 11:37:38 +0200

    Add CORS

--- Startup.cs
+++ Startup.cs
@@ -32,6 +32,12 @@ namespace DataGenerator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<SQLServerContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("SQLServerConnection")));
             services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("SQLServerConnection")));
@@ -59,12 +65,12 @@ namespace DataGenerator
             {
                 app.UseDeveloperExceptionPage();
             }
-          // else
-          //  {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            // else
+            //  {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             //   app.UseHsts();
             //}
-
+            app.UseCors("MyPolicy");
             app.UseHttpsRedirection();
             app.UseMiddleware<CustomExceptionMiddleware>();
             app.UseMvc();
