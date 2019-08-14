e91d0e994c64f9405973e130099d1685a6e02350
Kamil Bednarczyk <kamil2.bednarczyk@gmail.com>
2019-08-14 19:14:57 +0200

    Download data on service creation

--- ClientApp/src/app/services/column-types-getter.service.ts
+++ ClientApp/src/app/services/column-types-getter.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { ColumnTypeAdapter, ColumnType } from '../models/column-type';
 import { map } from 'rxjs/operators';
@@ -10,10 +10,17 @@ import { map } from 'rxjs/operators';
 export class ColumnTypesGetterService {
 
   private optionsUrl = "https://localhost:44361/api/generator/options";
+  private columnTypesObs = new Observable<Array<ColumnType>>();
 
-  constructor(private http: HttpClient, private columnTypeAdapter: ColumnTypeAdapter) { }
+  constructor(private http: HttpClient, private columnTypeAdapter: ColumnTypeAdapter) {
+    this.columnTypesObs = this.downloadColumnTypes();
+  }
+
+  downloadColumnTypes(): Observable<ColumnType[]> {
+    return this.http.get(this.optionsUrl).pipe(map((data: any[]) => data.map(item => this.columnTypeAdapter.adapt(item))));
+  }
 
   getColumnTypes(): Observable<ColumnType[]> {
-    return this.http.get(this.optionsUrl).pipe(map((data: any[]) => data.map(item => this.columnTypeAdapter.adapt(item))));
+    return this.columnTypesObs;
   }
 }
