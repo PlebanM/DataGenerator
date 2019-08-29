8edc277b984176ea38f8793faa55f41b43f4201c
Kamil Bednarczyk <kamil2.bednarczyk@gmail.com>
2019-08-29 23:00:09 +0200

    Add error messages to table

---
+++ ClientApp/src/app/services/error-message-provider.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
+import { stringify } from 'querystring';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorMessageProviderService {
+
+  private errorMessages: Map<string, string>;
+  private errorTemplateMap: Map<string, Function>
+
+  constructor() {
+
+    let fillTemplate = function (templateString, variables) {
+      return new Function("return `" + templateString + "`;").call(variables);
+    }
+
+    this.errorTemplateMap = new Map<string, Function>();
+    this.errorTemplateMap.set("min", fillTemplate);
+
+    this.errorMessages = new Map<string, string>();
+    this.errorMessages.set("required", "This field is required.");
+    this.errorMessages.set("notANumber", "Provide a number");
+    this.errorMessages.set("min", "Number must be greater than ${this.min.min}");
+    this.errorMessages.set("uniqueColumnName", "All column names must be unique");
+  }
+
+  getMessageForRandomError(errors: ValidationErrors): string {
+    if (errors == null) {
+      return "";
+    }
+    let propOfErr = Object.getOwnPropertyNames(errors);
+    if (propOfErr.length > 0) {
+      let err = this.errorMessages.get(propOfErr[0]);
+      let fillTemplate = this.errorTemplateMap.get(propOfErr[0]);
+      if (err && fillTemplate) {
+        return fillTemplate(err, errors);
+      }
+      return err ? err : "";
+    }
+    return "";
+  }
+
+
+}
--- ClientApp/src/app/services/options/common-validators.ts
+++ ClientApp/src/app/services/options/common-validators.ts
@@ -1,8 +1,14 @@
-import { ValidatorFn, Validators } from '@angular/forms';
+import { ValidatorFn, Validators, AbstractControl } from '@angular/forms';
+import { stringify } from 'querystring';
 
 export class CommonValidators {
 
     static getIsNumberValidator(): ValidatorFn {
         return Validators.pattern("\\d+");
+    }
+
+    static isNumber(control: AbstractControl) {
+        let regex = new RegExp('^-?\\d*$');
+        return regex.test(control.value) ? null : { notANumber: true };
     }
 }
